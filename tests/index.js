3111714bdb674aa35fcec76808ab9d8b2968dcfd
KaranRaul <karanraul02@gmail.com>
2024-12-21 12:32:42 +0530

    init

---
+++ tests/index.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+
+const BACKEND_URL = "http://localhost:3000"
+
+const fetchData = async () => {
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+        username: "teste@gmail.com",
+        password: "<PASSWORD>",
+        type: "admin"
+    });
+
+    console.log(response.data);
+};
+fetchData();
+
+
